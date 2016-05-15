71250e70f31b58e235d49cdc3c505f8f15024e53
Ty Kennington <tyjkenn@gmail.com>
2016-05-14 20:14:19 -0600

    Master updates continue

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
+      System.out.println("Master code");
       System.out.println("Hello world! This time with more!");
    }
 }
