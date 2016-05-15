984d76fc90b8b61e80fc53f12dd1f87f89bbc28f
Ty Kennington <tyjkenn@gmail.com>
2016-05-14 20:11:36 -0600

    Test branch test message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       System.out.println("Hello world! This time with more!");
+      System.out.println("Cool test branch stuff");
    }
 }
